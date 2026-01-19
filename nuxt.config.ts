660039bd7851262c90a7e56cbc31a4a947477600
villiogears <elphadeal@elphadeal.f5.si>
2026-01-19 11:12:25 +0000

    feat: update app title in nuxt.config.ts to 'elphadeal'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
-      title: 'aaa',
+      title: 'elphadeal',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
