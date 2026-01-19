088e43540d879e35088fbc154e00b20adb5049b6
villiogears <elphadeal@elphadeal.f5.si>
2026-01-19 11:07:14 +0000

    feat: update nuxt.config.ts to include app head configuration and remove favicon.ico

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/a11y',
     '@nuxt/content',
@@ -12,5 +12,16 @@ export default defineNuxtConfig({
     '@nuxt/scripts',
     '@nuxt/test-utils',
     '@nuxt/ui'
-  ]
+  ],
+  app: {
+    head: {
+      title: 'aaa',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' }
+      ],
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }]
+    }
+  }
 })
